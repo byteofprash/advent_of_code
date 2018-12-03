6a5d819632a9af6651d8768c76be0e44d9cecdd4
Prashanth Swaminathan <prash2912@gmail.com>
2018-12-03 13:52:27 +0100

    Completed day 3

---
+++ 2018/3/3_2.py
@@ -0,0 +1,21 @@
+import numpy as np
+lines = [line.rstrip('\n') for line in open('3.txt')]
+
+mat = np.zeros((1000,1000))
+for line in lines:
+    num,rest = line.split("@")
+    num = num.replace("#","").strip()
+    cols,rows = rest.strip().split(":")[0].split(",")
+    wide,tall = rest.strip().split(":")[1].split("x")
+    mat[int(cols):int(cols)+int(wide),int(rows):int(rows)+int(tall)]+=1
+
+
+for line in lines:
+    num,rest = line.split("@")
+    num = num.replace("#","").strip()
+    cols,rows = rest.strip().split(":")[0].split(",")
+    wide,tall = rest.strip().split(":")[1].split("x")
+    if np.all(mat[int(cols):int(cols)+int(wide),int(rows):int(rows)+int(tall)] == 1):
+        print(mat[int(cols):int(cols)+int(wide),int(rows):int(rows)+int(tall)])
+        print("NUm is:",num)
+
